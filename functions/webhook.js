dc66889154e883f31c2edcc02fa5ae3106132230
Stephan Smith <stephan@meenta.io>
2019-10-02 21:46:42 -0400

    Updated

--- functions/webhook.js
+++ functions/webhook.js
@@ -1,6 +1,48 @@
+
 exports.handler = function(event, context, callback) {
+
+		// 	{
+		//   "action": "content:create",
+		//   "meta": {
+		//     "projectId": "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+		//     "userId": "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+		//   },
+		//   "data": {
+		//     "contentId": "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+		//     "contentTypeName": "article",
+		//     "isSingleton": false
+		//   }
+		// }
+
+		const query = ` {
+			getCheese(_id: "${event.body.data.contentId}") {
+				_id
+				location {
+					city
+					state
+				}
+				milk
+				name
+				photo {
+					_id
+					caption
+					credit
+					description
+					filename
+					mimeType
+					path
+					sourceUrl
+					title
+					uploadStatus
+				}
+			}
+		}`;
+
+		console.log(query);
+		console.log('Got Data', event.body.action, event.body);
+
     callback(null, {
-    statusCode: 200,
-    body: "Webhook Handler"
+	    statusCode: 200,
+	    body: "Webhook Handler"
     });
 }
